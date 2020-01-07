fef00d1b5cd775402d5bbcba1a2e6541b4cce0ef
Yann L <yleroux@gmail.com>
2020-01-07 14:16:56 +0100

    1.1

--- entryDoor.lua
+++ entryDoor.lua
@@ -17,8 +17,8 @@ return {
 		devices = {
 		    44, -- Porte Jardin
 		    45, -- Porte Garage
-			46, -- Porte d'entrée
-			82, -- Dummy
+		    46, -- Porte d'entrée
+		    82, -- Dummy
 		},
 	},
 	logging = {
@@ -35,21 +35,21 @@ return {
 	        for i, id in ipairs(group) do
 	            if id == device.id then
 	                device_found = true
-                end
+                    end
 	        end
 	        
 	        if device_found then
-    	        if device._state == 'Open' then
-                    -- Previous state = Closed
-                    message = doorIcon..device.name..' is opened!'
-                end
+    	            if device._state == 'Open' then
+                        -- Previous state = Closed
+                        message = doorIcon..device.name..' is opened!'
+                    end
                     
-                if device._state == 'Closed' then
-                    -- Previous state = Open
-                    message = doorIcon..device.name..' is closed after '..device.lastUpdate.secondsAgo..' seconds'
-                end
-                dz.helpers.notif_telegram(dz, message) 
-            end
+                    if device._state == 'Closed' then
+                        -- Previous state = Open
+                        message = doorIcon..device.name..' is closed after '..device.lastUpdate.secondsAgo..' seconds'
+                    end
+                    dz.helpers.notif_telegram(dz, message) 
+            	end
 	    end
 	    
 	    local alarm_status = {}
@@ -63,21 +63,21 @@ return {
 	    alarm_status.AT_HOME = 'Arm Home'
 	    alarm_status.NONE    = 'Normal'
 	    
-        if device.changed then
-            local isAlarmed = dz.devices(75)._state
+            if device.changed then
+                local isAlarmed = dz.devices(75)._state
 
-            if isAlarmed == alarm_status.NONE then
-                sendStatusNotification(device, device_none)
-            end
+                if isAlarmed == alarm_status.NONE then
+                    sendStatusNotification(device, device_none)
+                end
             
-            if isAlarmed == alarm_status.AWAY then
-                sendStatusNotification(device, device_away)
-            end
+                if isAlarmed == alarm_status.AWAY then
+                    sendStatusNotification(device, device_away)
+                end
         
-            if isAlarmed == alarm_status.AT_HOME then
-                sendStatusNotification(device, device_home)    
-            end
-        end -- device.changed ?
+                if isAlarmed == alarm_status.AT_HOME then
+                    sendStatusNotification(device, device_home)    
+                end
+            end -- device.changed ?
         
 	end -- function execute
 }

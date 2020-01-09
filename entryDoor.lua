f4336d0d2265e87448d425e0a2ef66c45727e808
Yann L <yleroux@gmail.com>
2020-01-09 15:31:52 +0100

    1.2
    Use of standard constant instead of local ones

--- entryDoor.lua
+++ entryDoor.lua
@@ -1,7 +1,7 @@
 --[[ 
 author   = BreizhCat
 create   = 02.01.2020
-update   = 07.01.2020
+update   = 08.01.2020
 filename = entryDoor.lua
 repo     = https://github.com/BreizhCat/domoticz/blob/master/entryDoor.lua 
 
@@ -10,15 +10,15 @@ description = When the alarm is set on, the notification is send to telegram
 --]]
 
 local __script_name__ = "entryDoor"
-local __version__     = "1.1"
+local __version__     = "1.2"
 
 return {
 	on = {
 		devices = {
 		    44, -- Porte Jardin
 		    45, -- Porte Garage
-		    46, -- Porte d'entrée
-		    82, -- Dummy
+            46, -- Porte d'entrée
+            82, -- Dummy
 		},
 	},
 	logging = {
@@ -35,49 +35,43 @@ return {
 	        for i, id in ipairs(group) do
 	            if id == device.id then
 	                device_found = true
-                    end
+                end
 	        end
 	        
 	        if device_found then
-    	            if device._state == 'Open' then
-                        -- Previous state = Closed
-                        message = doorIcon..device.name..' is opened!'
-                    end
+    	        if device._state == 'Open' then
+                    -- Previous state = Closed
+                    message = doorIcon..device.name..' is opened!'
+                end
                     
-                    if device._state == 'Closed' then
-                        -- Previous state = Open
-                        message = doorIcon..device.name..' is closed after '..device.lastUpdate.secondsAgo..' seconds'
-                    end
-                    dz.helpers.notif_telegram(dz, message) 
-            	end
+                if device._state == 'Closed' then
+                    -- Previous state = Open
+                    message = doorIcon..device.name..' is closed after '..device.lastUpdate.secondsAgo..' seconds'
+                end
+                dz.helpers.notif_telegram(dz, message) 
+            end
 	    end
 	    
-	    local alarm_status = {}
-	   
 	    -- Device group
 	    local device_away = { 44, 45, 46, 82 }
 	    local device_home = { 44, 46, 82 }
 	    local device_none = { 82 }
-	    
-	    alarm_status.AWAY    = 'Arm Away'
-	    alarm_status.AT_HOME = 'Arm Home'
-	    alarm_status.NONE    = 'Normal'
-	    
-            if device.changed then
-                local isAlarmed = dz.devices(75)._state
 
-                if isAlarmed == alarm_status.NONE then
-                    sendStatusNotification(device, device_none)
-                end
+        if device.changed then
+            local isAlarmed = dz.devices(75)._state
+
+            if isAlarmed == dz.SECURITY_DISARMED then
+                sendStatusNotification(device, device_none)
+            end
             
-                if isAlarmed == alarm_status.AWAY then
-                    sendStatusNotification(device, device_away)
-                end
+            if isAlarmed == dz.SECURITY_ARMEDAWAY  then
+                sendStatusNotification(device, device_away)
+            end
         
-                if isAlarmed == alarm_status.AT_HOME then
-                    sendStatusNotification(device, device_home)    
-                end
-            end -- device.changed ?
+            if isAlarmed == dz.SECURITY_ARMEDHOME then
+                sendStatusNotification(device, device_home)    
+            end
+        end -- device.changed ?
         
 	end -- function execute
 }

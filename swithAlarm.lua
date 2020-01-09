40a89ecc601e247d0adf02ce58a72a4a169a42cf
Yann L <yleroux@gmail.com>
2020-01-09 15:42:41 +0100

    Update swithAlarm.lua

--- swithAlarm.lua
+++ swithAlarm.lua
@@ -5,7 +5,6 @@ update =
 filename = swithAlarm.lua
 repo   = https://github.com/BreizhCat/domoticz/blob/master/swithAlarm.lua
 description = Switch to bypass Alarm Screen
-
 --]]
 
 local __script_name__ = "swithAlarm"
@@ -16,19 +15,23 @@ return {
 		devices = { 86 }
 	},
 	execute = function(dz, device)
+	    local message = ''
 		if device._state == dz.SECURITY_ARMEDHOME then
-		    dz.log('Alarm: '..dz.SECURITY_ARMEDHOME)
+		    message = 'Alarm: '..dz.SECURITY_ARMEDHOME
 		    dz.devices(75).armHome()
 	    end
 	    
 		if device._state == dz.SECURITY_ARMEDAWAY then
-		    dz.log('Alarm: '..dz.SECURITY_ARMEDAWAY)
+		    message = 'Alarm: '..dz.SECURITY_ARMEDAWAY
 		    dz.devices(75).armAway()
 	    end
 	    
 		if device._state == dz.SECURITY_DISARMED then
-		    dz.log('Alarm: '..dz.SECURITY_DISARMED)
+		    message = 'Alarm: '..dz.SECURITY_DISARMED
 		    dz.devices(75).disarm()
 	    end
+	    
+	    dz.log(message)
+	    dz.helpers.notif_telegram(dz, message)
 	end
 }
